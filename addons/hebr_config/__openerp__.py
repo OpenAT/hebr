78273ab700a9e6a6913fd48d71fca7a405394d38
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:31:06 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/hebr_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'hebr_config',
+    'summary': '''FS-Online hebr instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance hebr
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
